bdb7866a5539efaadd23800a2d156e1bf0221ba8
Pedro J. Aguilar Vaides <65696918+PJBigBoss115@users.noreply.github.com>
2024-06-03 20:38:00 -0600

    Update 1_iniciarCamara.py

--- 1_iniciarCamara.py
+++ 1_iniciarCamara.py
@@ -2,6 +2,12 @@ import cv2 as cv
 import rospy
 from std_msgs.msg import Float64
 
+"""
+Este codigo inicia la camara ademas de inicializar los servos, se crea un nodo ros por lo
+cual debe de estar configurado de manera correcta, en mi caso no funciono ya que no he
+configurado el master pero ya configurando de manera correcta deberia funcionar sin problemas
+"""
+
 def move_servo(pan_pub, tilt_pub, pan_angle, tilt_angle):
     pan_pub.publish(pan_angle)
     tilt_pub.publish(tilt_angle)
@@ -17,16 +23,16 @@ if __name__ == "__main__":
 
     pan_angle = 0.0
     tilt_angle = 0.0
-    step_size = 0.1  # Ajusta el tamaÃ±o del paso segÃºn sea necesario
+    step_size = 0.1  # Ajusta el tamaño del paso segun sea necesario
 
     while frame.isOpened() and not rospy.is_shutdown():
-        ret, img = frame.read()  # Lee un frame de la cÃ¡mara
+        ret, img = frame.read()  # Lee un frame de la camara
         if not ret:
             break  # Si no se puede leer el frame, rompe el bucle
 
         cv.imshow('frame', img)  # Muestra el frame en una ventana llamada 'frame'
 
-        action = cv.waitKey(10) & 0xFF  # Espera 10 ms por una tecla y obtiene el cÃ³digo de la tecla presionada
+        action = cv.waitKey(10) & 0xFF  # Espera 10 ms por una tecla y obtiene el codigo de la tecla presionada
 
         if action == ord('q') or action == 113:  # Si se presiona 'q' o 'Q', sale del bucle
             break
@@ -39,12 +45,12 @@ if __name__ == "__main__":
         elif action == 83:  # Flecha hacia la derecha
             pan_angle += step_size
 
-        # Limita los Ã¡ngulos a un rango especÃ­fico (ajusta segÃºn sea necesario)
+        # Limita los angulos a un rango especifico (ajusta segun sea necesario)
         pan_angle = max(min(pan_angle, 1.57), -1.57)  # Limitar entre -90 y 90 grados
         tilt_angle = max(min(tilt_angle, 1.57), -1.57)  # Limitar entre -90 y 90 grados
 
         move_servo(pan_pub, tilt_pub, pan_angle, tilt_angle)  # Mueve los servos
 
-    frame.release()  # Libera la cÃ¡mara
+    frame.release()  # Libera la camara
     cv.destroyAllWindows()  # Cierra todas las ventanas de OpenCV
 
