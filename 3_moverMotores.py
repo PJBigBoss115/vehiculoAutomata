f6a5e79d3b1d729e0655b976e2102ec3be5a7b54
Pedro J. Aguilar Vaides <65696918+PJBigBoss115@users.noreply.github.com>
2024-06-03 20:46:08 -0600

    Update 3_moverMotores.py

--- 3_moverMotores.py
+++ 3_moverMotores.py
@@ -4,24 +4,24 @@ from Transbot_Lib import Transbot
 # Crear un objeto Transbot llamado bot
 bot = Transbot()
 
-# Iniciar la recepciÃ³n de datos, solo puede iniciarse una vez, todas las funciones de lectura de datos se basan en este mÃ©todo
+# Iniciar la recepción de datos, solo puede iniciarse una vez. Todas las funciones de lectura de datos se basan en este método.
 bot.create_receive_threading()
 
-# Habilitar el envÃ­o automÃ¡tico de datos
+# Habilitar el envío automático de datos
 enable = True
 bot.set_auto_report_state(enable, forever=False)
 
-# Deshabilitar el envÃ­o automÃ¡tico de datos
+# Deshabilitar el envío automático de datos
 enable = False
 bot.set_auto_report_state(enable, forever=False)
 
-# Limpiar los datos en cachÃ© enviados automÃ¡ticamente por el MCU
+# Limpiar los datos en caché enviados automáticamente por el MCU
 bot.clear_auto_report_data()
 
-# FunciÃ³n para mover los motores
+# Función para mover los motores
 def move_motors():
     print("Moviendo motores por 10 segundos...")
-    bot.set_car_motion(0.5, 0.0)  # Mover adelante a 50% de la velocidad mÃ¡xima
+    bot.set_car_motion(0.5, 0.0)  # Mover adelante a 50% de la velocidad máxima
     time.sleep(10)
 
     print("Deteniendo motores por 5 segundos...")
@@ -29,11 +29,11 @@ def move_motors():
     time.sleep(5)
 
     print("Moviendo motores por otros 10 segundos...")
-    bot.set_car_motion(0.5, 0.0)  # Mover adelante a 50% de la velocidad mÃ¡xima
+    bot.set_car_motion(0.5, 0.0)  # Mover adelante a 50% de la velocidad máxima
     time.sleep(10)
 
     print("Deteniendo motores...")
     bot.set_car_motion(0, 0)  # Detener los motores
 
-# Ejecutar la funciÃ³n de movimiento de motores
+# Ejecutar la función de movimiento de motores
 move_motors()
